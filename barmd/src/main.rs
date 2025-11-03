293deaff324671cd6619c9e9ad61c44b7ee1c98d
permania <0onesixed@gmail.com>
2025-11-03 11:55:50 -0800

    server: add systemd service and sample server

--- barmd/src/main.rs
+++ barmd/src/main.rs
@@ -4,8 +4,28 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::{error::Error, fs, io::{BufReader, BufRead}, os::unix::net::{UnixListener, UnixStream}};
 
-fn main() {
-    println!("Hello, world!");
+const SOCKET_PATH: &'static str = "/tmp/barmd.sock";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    if fs::exists(SOCKET_PATH)? {
+	fs::remove_file(SOCKET_PATH)?;
+    }
+
+    let listener = UnixListener::bind(SOCKET_PATH)?;
+
+    for stream in listener.incoming() {
+	handle_client(stream?)?;
+    }
+
+    Ok(())
+}
+
+fn handle_client(stream: UnixStream) -> Result<(), Box<dyn Error>> {
+    let stream = BufReader::new(stream);
+    for line in stream.lines() {
+        println!("{}", line?);
+    }
+    Ok(())
 }
